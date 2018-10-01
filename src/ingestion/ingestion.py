dffaddbf51eed4bb039015df67b895dee22d2453
pmrai@sandia.gov <pmrai@sandia.gov>
2018-10-01 09:57:02 -0700

    Added small snippets for plotting expected returns from random and risk managed strategy

--- src/ingestion/ingestion.py
+++ src/ingestion/ingestion.py
@@ -2,8 +2,6 @@ from glob import iglob
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#from bs4 import BeautifulSoup
-#import pandas_datareader.data as web
 import random
 import matplotlib.pyplot as plt
 import quandl
@@ -13,7 +11,7 @@ import os.path
 from pathlib import Path
 
 quandl.ApiConfig.api_key = 'GuTeqGsJBkf1sn8DAa9G'
-symbol = 'VIX'
+symbol = 'GOOGL'
 
 def get_data_path():
 	script_file = os.path.dirname(os.path.realpath('__file__'))
@@ -29,7 +27,7 @@ def get_quandl_data():
 	if(os.path.exists(read_file)):
 		print('%s_quandl.pk exists in the preprocessed folder'%(symbol))
 	else:
-		df = quandl.get("CBOE/"+symbol, start_date = "2014-12-31", end_date = "2018-08-20")
+		df = quandl.get("WIKI/"+symbol, start_date = "2010-12-31", end_date = "2018-08-20")
 		write_path = os.path.join(data_dir_path,'preprocessed')
 		write_file = os.path.join(write_path,'%s_quandl.pk'%(symbol))
 		df.to_pickle(write_file)
@@ -46,7 +44,7 @@ def extract_symbol_from_data():
 	else:
 		raw_data_dir_path = os.path.join(data_dir_path,'raw')
 		symbol_df = pd.DataFrame()
-		for ind in range(12,13):
+		for ind in range(1,13):
 			month_csv = r'2017_%s/*.csv'%(str(ind))
 			dr = os.path.join(raw_data_dir_path,month_csv)
 			print(dr)
@@ -59,7 +57,6 @@ def extract_symbol_from_data():
 		write_path = os.path.join(data_dir_path,'preprocessed')
 		write_file = os.path.join(write_path,'%s.pk'%(symbol))
 		symbol_df.to_pickle(write_file)
-	#symbol_df.to_csv('%s.csv'%(symbol))
 
 def clean_options_data():
 	data_dir_path = get_data_path()
@@ -83,8 +80,6 @@ def clean_options_data():
 	    write_path = os.path.join(data_dir_path,'preprocessed')
 	    write_file = os.path.join(write_path,'%s_clean.pk'%(symbol))
 	    df.to_pickle(write_file)
-	    #df.to_pickle('%s_clean.pk'%(symbol))
-	    #df.to_csv('%s_clean.csv'%(symbol))
     
 
 def asset_value_on_expiry():
@@ -97,13 +92,13 @@ def asset_value_on_expiry():
 		read_file = os.path.join(read_path,'%s_clean.pk'%(symbol))
 		df = pd.read_pickle(read_file)
 		expiration_dates = df.ExpirationDate.unique()
-		qdl_pd = pd.read_pickle('%s_quandl.pk'%(symbol))
+		qdl_pd = pd.read_pickle(os.path.join(read_path,'%s_quandl.pk'%(symbol)))
 		df_with_expiry = pd.DataFrame()
 		market_value_dict = {}
 		for date in expiration_dates:
 			date = pd.to_datetime(date)
 			if date in qdl_pd.index:
-				value = qdl_pd.loc[date]['VIX Close']	
+				value = qdl_pd.loc[date]['Close']	
 				market_value_dict[date] = value
 			else:
 				df = df.drop(df[df.ExpirationDate == date].index)
@@ -124,7 +119,7 @@ def get_profit_on_options():
 		df = pd.read_pickle(read_file)
 		df['P_L'] = [max(-x,y-(x+z)) for x,y,z in zip(df.LastPrice, df.ActualMarketValue, df.StrikePrice)]
 		df = df.sort_values('Days')
-		df = df[['DataDate','ExpirationDate','ImpliedVolatility','Delta','Vega','P_L','Days']]
+		df = df[['DataDate','ExpirationDate','LastPrice','ImpliedVolatility','Delta','Vega','P_L','Days']]
 		write_path = os.path.join(data_dir_path,'preprocessed')
 		write_file = os.path.join(write_path,'%s_with_profit.pk'%(symbol))
 		df.to_pickle(write_file)
@@ -138,8 +133,6 @@ def sort_profit_data_date():
 	else:
 		read_file = os.path.join(read_path,'%s_with_profit.pk'%(symbol))
 		df = pd.read_pickle(read_file)
-		#df = df.set_index(['Days'], inplace=True)
-		#df = df.reset_index(drop = True)
 		print(df)
 		df = df.groupby('Days').apply(lambda x: x.sort_values(['DataDate']))
 		df.reset_index(drop = True, inplace = True)
@@ -150,65 +143,13 @@ def sort_profit_data_date():
 		df.to_csv('test.csv')
 
 def extract_data_for_model_building():
-	get_quandl_data()
-	# quandl_df = pd.read_pickle('%s_quandl.pk'%(symbol))
-	# print(quandl_df)
-	extract_symbol_from_data()
-	# symbol_df = pd.read_pickle('%s.pk'%(symbol))
-	clean_options_data()
-	# clean_symbol_df = pd.read_pickle('%s_clean.pk'%(symbol))
 	asset_value_on_expiry()
 	get_profit_on_options()
 	sort_profit_data_date()
-	# df = pd.read_pickle('%s_with_expiry.pk'%(symbol))
-	# df['P_L'] = [max(-x,y-(x+z)) for x,y,z in zip(df.LastPrice, df.ActualMarketValue, df.StrikePrice)]
-	# df = df.sort_values('Days')
-	# df.to_pickle('%s_with_pl.pk'%(symbol))
-	# df.to_csv('%s_with_pl.csv'%(symbol))
-	# df = pd.read_pickle('%s_with_pl.pk'%(symbol))
+
 
 extract_data_for_model_building()	
 
-# df = df.loc[df['Days'] == 1]
-
-# df = df.reset_index(drop=True)
-
-
-# df_train=df.sample(frac=0.5,random_state=200)
-# df_test=df.drop(df_train.index)
-
-# X_train = df_train[['Delta','Vega']]
-# y_train = df_train[['P_L']]
-
-# X_test = df_test[['Delta','Vega']]
-# y_test = df_test[['P_L']]
-
-# lm = linear_model.LinearRegression()
-# model = lm.fit(X_train,y_train)
-# predictions = lm.predict(X_test)
-# df_test['predictions'] = predictions
-# print(df_train)
-# print(df_test)
-# # Model comparison
-# df_test_random = df_test.sample(frac=0.2, random_state=100)
-# df_test_predicted = df_test.nlargest(20, ['predictions'], keep='first')
-
-
-# print(df_test_random)
-# print(df_test_predicted)
-# df_comparison = pd.DataFrame()
-
-# df_test_random =  df_test_random.reset_index(drop=True)
-# df_test_predicted = df_test_predicted.reset_index(drop=True)
-
-# df_comparison['random strategy'] = df_test_random['P_L']
-# df_comparison['risk managed strategy'] = df_test_predicted['P_L']
-# print(df_comparison)
-
-# fig1 = plt.figure()
-# df_comparison.plot.kde()
-# plt.show()
-# fig1.savefig('strategy_comp.png')
 
 
 
--- src/model/regression_model.py
+++ src/model/regression_model.py
@@ -11,9 +11,12 @@ from scipy.stats import norm
 from sklearn import linear_model
 import os.path
 from pathlib import Path
+import matplotlib.animation as animation
+import time
 
 #extract_data_for_model_building()
-symbol = 'VIX'
+symbol = 'GOOGL'
+inv_date = pd.to_datetime('2017-08-01')
 
 def regression_model():
 	extract_data_for_model_building()
@@ -22,21 +25,20 @@ def regression_model():
 	read_file = os.path.join(read_path,'%s.pk'%(symbol))
 	df_all = pd.read_pickle(read_file)
 	list_of_days = df_all.Days.unique()
+	days = []
+	mean_random = []
+	std_random = []
+	mean_managed = []
+	std_managed = []
+	df_plot = pd.DataFrame()
 	print(list_of_days)
-	for day in list_of_days[1:20]:
-		#df = pd.DataFrame()
+	for day in list_of_days[30:60]:
 		df = df_all.loc[df_all['Days'] == day]
 
 
-		#df = df.reset_index(drop=True)
-
-		#df = df.set_index('DataDate')
-		inv_date = pd.to_datetime('2017-03-01')
 		df_train = df[df['DataDate']<inv_date]
 		df_test = df[df['DataDate']>inv_date] 
-		#print(df[df['DataDate']>inv_date])
-		#df_train = df.sample(frac=0.5,random_state=200)
-		#df_test = df.drop(df_train.index)
+
 		if df_train.empty is False  and df_test.empty is False:  
 			X_train = df_train[['Delta','Vega']]
 			y_train = df_train[['P_L']]
@@ -50,32 +52,50 @@ def regression_model():
 			df_test['predictions'] = predictions
 			
 			# Model comparison
-			df_test_random = df_test.sample(20, random_state=100)
-			#df_test_maxvol = df_test.nlargest(20,['Volume'], keep='first')
-			#print(df_test_maxvol)
-			df_test_predicted = df_test.nlargest(20,['predictions'], keep='first')
-			df_test_vol = df_test.nlargest(20,['Volume'], keep='first')
-			print(df_test_vol[['P_L','Volume','predictions']])
+			df_test_random = df_test.sample(10, random_state=100)
 
-			# print(df_test_random)
-			# print(df_test_predicted)
+			df_test_predicted = df_test.nlargest(10,['predictions'], keep='first')
+
 			df_comparison = pd.DataFrame()
+			df_percent_profit = pd.DataFrame()
 
 			df_test_random =  df_test_random.reset_index(drop=True)
 			df_test_predicted = df_test_predicted.reset_index(drop=True)
-			df_test_volume = df_test_vol.reset_index(drop=True)
+
 
 			df_comparison['random strategy'] = df_test_random['P_L']
 			df_comparison['risk managed strategy'] = df_test_predicted['P_L']
-			df_comparison['max vol strategy'] = df_test_volume['P_L']
-			#df_comparison['max vol trategy'] = df_test_maxvol['P_L']
+
+			df_percent_profit['P_L_Percent_Managed'] =  (df_test_predicted['P_L']/df_test_predicted['LastPrice'])*100
+			df_percent_profit['P_L_Percent_Random'] =  (df_test_random['P_L']/df_test_random['LastPrice'])*100
+
 			print(df_comparison)
 
-			#fig1 = plt.figure()
-			df_comparison.plot.kde()
-			plt.pause(0.05)
-			plt.draw()
-	plt.show()	
-			#fig1.savefig('strategy_comp.png')
 
+			days.append(day)
+			mean_random.append(df_percent_profit['P_L_Percent_Random'].mean())
+			std_random.append(df_percent_profit['P_L_Percent_Random'].std())
+			mean_managed.append(df_percent_profit['P_L_Percent_Random'].mean())
+			std_managed.append(df_percent_profit['P_L_Percent_Random'].std())
+
+
+			# df_comparison.plot.kde(xlim=(-100,100))
+			# plt.pause(0.05)
+			# plt.xlabel('Expected Return')
+			# plt.draw()
+	#plt.show()	
+	font = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 16,
+        }
+	df = {'Days':days,'mean_random':mean_random,'std_random':std_random}
+	df_plot = pd.DataFrame(df)
+	df_plot = df_plot.set_index(['Days'])
+	print(df_plot)
+	plt.errorbar(df_plot.index, df_plot['mean_random'], yerr=df_plot['std_random'])
+	plt.title('Options Investment in Google (Random Strategy)', fontdict=font)
+	plt.xlabel('Investment Duration (Days)', fontdict=font)
+	plt.ylabel('Expected Return (Percentage)', fontdict=font)
+	plt.show()
 regression_model()
